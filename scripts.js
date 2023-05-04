8b813ea9727a5d91b49a33c209b27a91b2fa90b5
phbvy <83098847+phbvy@users.noreply.github.com>
2023-05-05 01:19:26 +0900

    add chat sandbox

--- scripts.js
+++ scripts.js
@@ -12,3 +12,32 @@ function slide() {
 }
 
 window.addEventListener("scroll", slide);
+
+function createChatBubble(comment) {
+    if (comment != "") {
+        const messageBubble = document.createElement("div");
+        messageBubble.innerHTML += comment;
+        messageBubble.classList.add("bg-blue-800", "rounded-xl", "overflow-hidden", "p-4", "col-start-3", "col-span-2",  "border", "border-slate-100", "text-slate-100", "text-3xl", "font-bold","animate", "slide", "italic");
+        return messageBubble;
+    }
+}
+
+function addChatBubble(chatbubble) {
+    const messageContainer = document.querySelector("#displaymessagecontainer");
+    messageContainer.appendChild(chatbubble);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.querySelector("#displaymessage")
+    const createCommentForm = document.querySelector("#commentform")
+      
+    createCommentForm.addEventListener("submit", (e) => {
+      e.preventDefault()
+      let comment = e.target.usermesssage.value;
+      const chatbubble = createChatBubble(comment);
+      addChatBubble(chatbubble);
+      createCommentForm.reset();
+      document.querySelector("#sandboxwarning").classList.remove('hidden');
+    })
+  })   
