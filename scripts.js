991236c640c9fcb951aae730317952d0444c4f83
phbvy <phbvy.g@gmail.com>
2026-04-07 15:23:29 +0200

    updated animation

--- scripts.js
+++ scripts.js
@@ -25,13 +25,27 @@ function createChatBubble(comment) {
 
 function addChatBubble(chatbubble) {
     const messageContainer = document.querySelector("#displaymessagecontainer");
+    chatbubble.classList.remove("slide");
     if (messageContainer.children.length==1) {
         const messageContainer2 = document.querySelector("#displaymessagecontainer2")
         messageContainer2.appendChild(chatbubble)
     } else {
         messageContainer.appendChild(chatbubble);
     }
+    requestAnimationFrame(() => {
+        requestAnimationFrame(() => {
+            chatbubble.classList.add("slide");
+        });
+    });
 };
+
+function typingEffect() {
+    const bubble = document.getElementById('typingBubble');
+    if (bubble.classList.contains('hidden')) {
+        bubble.classList.remove('hidden');
+        bubble.classList.add('show');
+    }
+}
 
 function scrollTopVisibility() {
     var button = document.getElementById('backToTopBtn');
@@ -46,15 +60,28 @@ window.addEventListener("scroll", scrollTopVisibility);
 
 document.addEventListener('DOMContentLoaded', () => {
     scrollTopVisibility();
+    slide();
 
     document.querySelector("#displaymessage")
     const createCommentForm = document.querySelector("#commentform")
       
+    const input = document.getElementById('messagecontent');
+    input.addEventListener('blur', () => {
+        if (input.value.trim() === '') {
+            const bubble = document.getElementById('typingBubble');
+            bubble.classList.add('hidden');
+            bubble.classList.remove('show');
+        }
+    });
+
     createCommentForm.addEventListener("submit", (e) => {
       e.preventDefault()
       let comment = e.target.usermesssage.value;
       const chatbubble = createChatBubble(comment);
       addChatBubble(chatbubble);
+      const bubble = document.getElementById('typingBubble');
+      bubble.classList.add('hidden');
+      bubble.classList.remove('show');
       createCommentForm.reset();
 
       setTimeout(()=> {const warnings = document.querySelectorAll(".sandboxwarning")
